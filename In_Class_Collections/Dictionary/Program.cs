666dce5d3386969c8bbb21b0510892bc9d1ae872
ghern <ghern@DESKTOP-OCUVVH0>
2021-10-05 11:34:03 -0500

    In class completed

--- In_Class_Collections/Dictionary/Program.cs
+++ In_Class_Collections/Dictionary/Program.cs
@@ -12,6 +12,38 @@ namespace Dictionary
             studentRecords.Add(2, 3.5);
             studentRecords.Add(3, 4.0);
 
+            foreach (int id in studentRecords.Keys)
+            {
+                Console.WriteLine(id);
+            }
+            Console.WriteLine();
+            string answer;
+            
+            do
+            {
+                Console.WriteLine("Do you wnat to add another student to our records? yes or no? >>");
+                answer = Console.ReadLine();
+
+                if (answer.ToLower() == "yes")
+                {
+                    Console.WriteLine("What is the ID?");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("What is the GPA? >>");
+                    double gpa = Convert.ToDouble(Console.ReadLine());
+
+                    if (studentRecords.ContainsKey(id) == true)
+                    {
+                        studentRecords[id] = gpa;
+                    }
+                    else
+                    {
+                        Console.WriteLine("THat student already exists, so we are updating their GPA");
+                        studentRecords.Add(id, gpa);
+                    }
+                }
+
+            } while (answer.ToLower() == "yes");
+            
             Console.WriteLine("Which student do you wnat to see the GPA off? >>");
             int studentID = Convert.ToInt32(Console.ReadLine());
 
